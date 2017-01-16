aa617b20deacaa599d118a5d5f0a11e565702719
Mohamad Yassine <mohamad.yassine@carleton.ca>
2017-01-16 14:55:24 -0500

    Updated for publc use

--- appChrome.sh
+++ appChrome.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-url=$((type -P xclip) -sel clip -o)
-url2=$(xclip -o)
-echo "$url"
-echo "$url2"
+##############################################################################################
+# I hate space that is not used efficiently.
+# Using Chrome's app mode or also called kiosk mode. I can use my whole screen real estate to
+# watch videos, read documents or do leasure browsing, without the unused toolbars or tabs.
+# This could be for you or could not be, but it's mine and for I love it.
+# Works very well along i3 or bspwm
+#
+# REQUIRES xclip
+##############################################################################################
+#stores clipboard into url
+url=$(xclip -o)
+
+#uses url to open chrome (or whichever browser you prefer) in app mode.
 exec /usr/bin/google-chrome --app="$url2"
-google-chrome --app="https://www.cyberciti.biz/faq/unix-linux-bsd-appleosx-bash-assign-variable-command-output/"
